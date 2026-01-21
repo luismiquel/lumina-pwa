bfd31c0dea7c4667e10e0e5174864d4bf51ec81b
Luis Miguel <luismiguelgarciadelasmorenas@gmail.com>
2026-01-21 20:14:38 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,30 @@
-﻿import { defineConfig } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { fileURLToPath, URL } from "node:url";
 
 export default defineConfig({
+  base: "/",
+
   plugins: [react()],
+
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url))
     }
   },
-  server: { port: 3000, host: true },
-  build: { outDir: "dist", sourcemap: false }
+
+  server: {
+    port: 3000,
+    host: true
+  },
+
+  preview: {
+    port: 3000
+  },
+
+  build: {
+    outDir: "dist",
+    sourcemap: false,
+    target: "es2020"
+  }
 });
